030bd9fae8d9c6ac1aa2d88870b32071e29872f8
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-07-24 09:55:19 +0545

    Update File_read.py

--- File_read.py
+++ File_read.py
@@ -2,20 +2,20 @@
 
 
 # 1 accessing using open and passing file name and mode
-# f  = open("demo.txt" ,"r")
-# data = f.read() #reading the data
-# print(data) #printing the data
-# print(type(data)) #Printing the data types
-# f.close() # Closing the operation
+f  = open("demo.txt" ,"r")
+data = f.read() #reading the data
+print(data) #printing the data
+print(type(data)) #Printing the data types
+f.close() # Closing the operation
 
 
 
 # 2 Reading only specific character
 
-# f = open("demo.txt","r")
-# data = f.read(5) #reading only 5 character from a file
-# print(data)
-# f.close()
+f = open("demo.txt","r")
+data = f.read(5) #reading only 5 character from a file
+print(data)
+f.close()
 
 # 3 Reading only one line at a time
 f = open("demo.txt","r")
