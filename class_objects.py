d1fc3f168251d86fccf2a681de305d8b753227b3
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-07-25 09:01:24 +0545

    Classes and objects in Python.

---
+++ class_objects.py
@@ -0,0 +1,32 @@
+#Class is a blueprint for creating  objects
+
+
+# 1.
+#Creating a class
+
+class Student:
+    name = "Sandesh"
+
+
+#Creating an object
+
+s1 = Student()
+print(s1.name)
+
+# 2.
+
+#class
+class Car:
+    color = "red"
+    brand = "BMW"
+    price = 200000
+
+
+#object
+car1 = Car()
+print(car1.color)
+print(car1.brand)
+print(car1.price)
+
+
+
