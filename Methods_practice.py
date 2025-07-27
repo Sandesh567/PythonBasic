eb0defd9bc24938e949c9bc469769983a6191fc2
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-07-27 09:50:40 +0545

    Update Methods_practice.py

--- Methods_practice.py
+++ Methods_practice.py
@@ -19,3 +19,30 @@ s1 = Student("sandesh",[87,88,90])
 s1.avg_marks()
 
 
+# Directly changing the attribute
+s1.name = "Sandesh Raj Bhatta"
+s1.avg_marks()
+
+
+#Static Methods - Methods that do not use the self parameters
+
+class Car:
+    def __init__(self,brand,price):
+        self.brand = brand
+        self.price = price
+
+    #static method
+    @staticmethod #Decorator-extends the behaviour without modifying it
+    def engine():
+        print("vrooom vrommmm !")
+
+
+
+c1 = Car("Mercedes", 300000000)
+
+print(c1.brand, c1.price)
+
+c1.engine()
+
+
+
