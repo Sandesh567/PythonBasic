688a66c87ccd8e8b8b83b3e276395e86e7d662bb
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-07-29 10:02:15 +0545

    Update statements.py

--- statements.py
+++ statements.py
@@ -11,3 +11,14 @@ else:
     print("wear warm clothes")
 
 # print("this is a different block")
+
+
+#Example
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
